892477f029bdc5d6111aee99bfbe2ac4d2d0e293
Alexmnzlms <alexmanza2@gmail.com>
2022-04-24 18:39:44 +0200

    Compute by axis not by map

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream> 
 #include <string>   
 #include <cmath>    
-#include <cstdlib>  
+#include <cstdlib>
+#include "aux.h"
 #include "malla3d.h"
 #include "panorama.h"
 
@@ -13,18 +14,24 @@ int main() {
 	malla.load_obj("models/214_izq_posicionado/214_izq_posicionado.obj");
 	malla.export_obj("models/214_izq_posicionado/214_izq_posicionado_reduct.obj");
 
-	// malla.load_obj("models/214_izq_posicionado/cubo.obj");
-	// malla.export_obj("models/214_izq_posicionado/cubo_reduct.obj");
+	// malla.load_obj("models/214_izq_posicionado/214_izq_posicionado_simple.obj");
+	// malla.export_obj("models/214_izq_posicionado/214_izq_posicionado_simple_reduct.obj");
+	
+	// malla.load_obj("models/214_izq_posicionado/cube.obj");
+	// malla.export_obj("models/214_izq_posicionado/cube_reduct.obj");
 
 	malla.calculatePanorama(SDM, X, 0.5, 4);
-	malla.calculatePanorama(SDM, Y, 0.5, 4);
-	malla.calculatePanorama(SDM, Z, 0.5, 4);
-
 	malla.calculatePanorama(NDM, X, 0.5, 4);
-	malla.calculatePanorama(NDM, Y, 0.5, 4);
-	malla.calculatePanorama(NDM, Z, 0.5, 4);
+	combine_panorama(X);
 
-	combine_panorama();
+	malla.calculatePanorama(SDM, Y, 0.5, 4);
+	malla.calculatePanorama(NDM, Y, 0.5, 4);
+	combine_panorama(Y);
+
+	malla.calculatePanorama(SDM, Z, 0.5, 4);
+	malla.calculatePanorama(NDM, Z, 0.5, 4);
+	combine_panorama(Z);
+	
 
 	return 0;
 }
