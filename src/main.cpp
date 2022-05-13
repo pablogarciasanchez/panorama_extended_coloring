2146bf102170035ee63ca63f88f58d83a1d95e28
Alexmnzlms <alexmanza2@gmail.com>
2022-05-13 12:34:37 +0200

    Update main to include new output folder

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,17 @@
 
 int main(int argc, char * argv[]) {
 
-	if(argc != 4){
+	if(argc != 5){
 		std::cout << "Wrong parameters" << std::endl;
 		std::cout << "bin/panorama_extended [model name] [relive route to 3D model]"
-		<< " [output folder PANORAMA extended]"  << std::endl;
+		<< " [output folder PANORAMA extended] [output folder feature map]"  << std::endl;
 		exit(-1);
 	}
 
 	std::string name = argv[1];
 	std::string path = argv[2];
 	std::string output_folder_pe = argv[3];
+	std::string output_folder_fm = argv[4];
 
 	std::cout << "Loading " << name << "\tPath: " << path << "..." << std::endl;
 
@@ -34,21 +35,19 @@ int main(int argc, char * argv[]) {
 	malla.calculate_panorama(SDM, X, 0.5, 4);
 	malla.calculate_panorama(NDM, X, 0.5, 4);
 
-	malla.combine_panorama(X,output_folder_pe,false);
-
 	malla.calculate_panorama(SDM, Y, 0.5, 4);
 	malla.calculate_panorama(NDM, Y, 0.5, 4);
-
-	malla.combine_panorama(Y,output_folder_pe,false);
 
 	malla.calculate_panorama(SDM, Z, 0.5, 4);
 	malla.calculate_panorama(NDM, Z, 0.5, 4);
 
+	malla.combine_panorama(X,output_folder_pe,false);
+	malla.combine_panorama(Y,output_folder_pe,false);
 	malla.combine_panorama(Z,output_folder_pe,false);
 
-	malla.concat_panorama(SDM,output_folder_pe,false);
-	malla.concat_panorama(NDM,output_folder_pe,false);
-	malla.concat_panorama(GNDM,output_folder_pe,false);
+	malla.concat_panorama(SDM,output_folder_fm,false);
+	malla.concat_panorama(NDM,output_folder_fm,false);
+	malla.concat_panorama(GNDM,output_folder_fm,false);
 	
 	return 0;
 }
