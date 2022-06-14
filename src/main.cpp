acb7352465d9ecaa74c0684b95dd42f04a0c65b5
Alexmnzlms <alexmanza2@gmail.com>
2022-06-14 12:18:21 +0200

    New main for data augment

--- src/main.cpp
+++ src/main.cpp
@@ -28,9 +28,10 @@ int main(int argc, char * argv[]) {
 
 	std::cout << "Loaded " << malla.get_name() << std::endl;
 
-	
+	std::string rot_name = malla.get_name() + "_0";
+	malla.set_name(rot_name);
 
-	malla.mesh_pose_norm();
+	// malla.mesh_pose_norm();
 
 	malla.calculate_panorama(SDM, X, 1, 4);
 	malla.calculate_panorama(NDM, X, 1, 4);
@@ -48,6 +49,48 @@ int main(int argc, char * argv[]) {
 	malla.concat_panorama(SDM,output_folder_fm,false);
 	malla.concat_panorama(NDM,output_folder_fm,false);
 	malla.concat_panorama(GNDM,output_folder_fm,false);
+
+	int cont = 0;
+	for(int ax = 0; ax < 3; ax++){
+		std::cout << "Loading " << name << "\tPath: " << path << "..." << std::endl;
+
+		Malla3D malla(name, path);
+
+		std::cout << "Loaded " << malla.get_name() << std::endl;
+		std::string orig_name = malla.get_name();
+
+		int ind = 1;
+		float angle = 15.0;
+		for(float i = angle; i <= 180.0; i += angle, ind++){
+
+			std::string rot_name = orig_name + "_" + std::to_string(cont);
+			malla.set_name(rot_name);
+			cont++;
+
+			std::cout << "Rotando en el eje: " << axis_to_string((Axis)ax)
+			<< " un angulo de: " << ind*angle << std::endl;
+			std::cout << "ind " << ind << std::endl;
+			std::cout << "angle " << angle << std::endl;
+
+			malla.rotate_mesh((Axis)ax,angle*(M_PI/180.0));
+			malla.calculate_panorama(SDM, X, 1, 4);
+			malla.calculate_panorama(NDM, X, 1, 4);
+
+			malla.calculate_panorama(SDM, Y, 1, 4);
+			malla.calculate_panorama(NDM, Y, 1, 4);
+
+			malla.calculate_panorama(SDM, Z, 1, 4);
+			malla.calculate_panorama(NDM, Z, 1, 4);
+
+			malla.combine_panorama(X,output_folder_pe,false);
+			malla.combine_panorama(Y,output_folder_pe,false);
+			malla.combine_panorama(Z,output_folder_pe,false);
+
+			malla.concat_panorama(SDM,output_folder_fm,false);
+			malla.concat_panorama(NDM,output_folder_fm,false);
+			malla.concat_panorama(GNDM,output_folder_fm,false);			
+		}
+	}
 	
 	return 0;
 }
